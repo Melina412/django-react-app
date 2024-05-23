f40e1bb83040365492a590637933909d4ac53af3
Melina412 <melina.schild@icloud.com>
2024-05-23 10:47:40 +0200

    browser router setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,39 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function Logout() {
+  localStorage.clear();
+  return <Navigate to='/login' />;
+}
+
+// nach register local storage leeren um fehler durch abgelaufene access token zu vermeiden
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register />;
+}
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
