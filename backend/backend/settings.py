15604c5e0738f3ed693ab6d9ccfc8708aabde36e
Melina412 <melina.schild@icloud.com>
2024-05-23 16:31:52 +0200

    db update & backend deployment settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'USER': os.getenv('DB_USER'),
+        'NAME': os.getenv('DB_NAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
 
