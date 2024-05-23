7da02a7bb9f1ac0944c9e1ede33d2269179a9233
Melina412 <melina.schild@icloud.com>
2024-05-23 18:00:13 +0200

    backend url updated

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
 
-const apiUrl = '/choreo-apis/awbo/backend/rest-api-be2/v1.0';
+const apiUrl = '/choreo-apis/django-x-react/backend/v1'; // connection zum backend in choreo deployment environment
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
